d113705309a4387a02ff95b2c3ea8c614a70b0e6
이상경 <sknglee22@naver.com>
2020-06-06 19:00:29 +0900

    list_editor 자리복귀

--- project/app/views.py
+++ project/app/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
+    comments_editor = Post_youtuber.objects.all
+    posts_editor = Post_editor.objects.all
+    return render(request, 'home.html', {'posts_youtuber' : posts_youtuber, 'posts_editor' : posts_editor})
+
+def list_editor(request):
     posts_youtuber = Post_youtuber.objects.all
     posts_editor = Post_editor.objects.all
     return render(request, 'home.html', {'posts_youtuber' : posts_youtuber, 'posts_editor' : posts_editor})
+
